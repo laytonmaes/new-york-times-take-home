935a4eb3d22e14fc78c18c039c7668b68c854ea3
Dylan Duke <laytonmaes@gmail.com>
2022-09-14 19:23:34 -0600

    Add mock data file

---
+++ src/mockData.js
@@ -0,0 +1,69 @@
+const mockData = [
+    {
+        section: "us",
+        subsection: "politics",
+        title: "Biden Maneuvers to Try to Avoid Devastating Rail Strike",
+        abstract: "The Biden administration is considering executive action to try to avoid a shutdown of the nation’s rail network that would harm the economy ahead of the midterm elections.",
+        url: "https://www.nytimes.com/2022/09/14/us/politics/rail-strike-biden.html",
+        uri: "nyt://article/847c4f95-7326-5f1a-b8e5-1ff45b25db00",
+        byline: "By Jim Tankersley and Ana Swanson",
+        item_type: "Article",
+        updated_date: "2022-09-14T18:35:48-04:00",
+        created_date: "2022-09-14T17:48:53-04:00",
+        published_date: "2022-09-14T17:48:53-04:00",
+        material_type_facet: "",
+        kicker: "",
+        des_facet: [
+        "Railroads",
+        "Labor and Jobs",
+        "Organized Labor",
+        "Strikes"
+        ],
+        org_facet: [
+        "Amtrak",
+        "Assn of American Railroads",
+        "BNSF Railway",
+        "CSX Corporation",
+        "International Assn of Machinists and Aerospace Workers",
+        "Norfolk Southern Corporation",
+        "Union Pacific Corporation"
+        ],
+        per_facet: [],
+        geo_facet: [],
+        multimedia: [
+        {
+        url: "https://static01.nyt.com/images/2022/09/14/business/14dc-biden-rail-1/merlin_208570593_8ffc5435-a6d0-433f-9a33-dd6974615cdb-superJumbo.jpg",
+        format: "Super Jumbo",
+        height: 1366,
+        width: 2048,
+        type: "image",
+        subtype: "photo",
+        caption: "American railways are responsible for transporting much of the chemicals, automobiles, metals, grain and oil that moves around the continent.",
+        copyright: "Gene J. Puskar/Associated Press"
+        },
+        {
+        url: "https://static01.nyt.com/images/2022/09/14/business/14dc-biden-rail-1/merlin_208570593_8ffc5435-a6d0-433f-9a33-dd6974615cdb-threeByTwoSmallAt2X.jpg",
+        format: "threeByTwoSmallAt2X",
+        height: 400,
+        width: 600,
+        type: "image",
+        subtype: "photo",
+        caption: "American railways are responsible for transporting much of the chemicals, automobiles, metals, grain and oil that moves around the continent.",
+        copyright: "Gene J. Puskar/Associated Press"
+        },
+        {
+        url: "https://static01.nyt.com/images/2022/09/14/business/14dc-biden-rail-1/14dc-biden-rail-1-thumbLarge.jpg",
+        format: "Large Thumbnail",
+        height: 150,
+        width: 150,
+        type: "image",
+        subtype: "photo",
+        caption: "American railways are responsible for transporting much of the chemicals, automobiles, metals, grain and oil that moves around the continent.",
+        copyright: "Gene J. Puskar/Associated Press"
+        }
+        ],
+        short_url: "https://nyti.ms/3xo0maa"
+        }
+]
+
+export default mockData;
